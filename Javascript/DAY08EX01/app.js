1b7a0dd195090f1924689ea401e3b2ddb257b5b8
jhyunwoo <tiers.edamame0z@icloud.com>
2022-05-24 15:43:11 +0900

    DAY07EX04 pre

--- Javascript/DAY08EX01/app.js
+++
@@ -1,5 +0,0 @@
-const userName = prompt("이름을 입력해주세요.");
-const nameLoc = document.querySelector("#userName");
-const createName = document.createElement("h1");
-createName.innerText = userName + "님 안녕하세요.";
-nameLoc.appendChild(createName);
