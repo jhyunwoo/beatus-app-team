ca3577dce41628cea2286d80828465401391e37a
전현우 <jhyunwoo@Ampersand.local>
2022-06-06 01:52:07 +0900

    rn day 6 ex 1 question

---
+++ React-Native/DAY06EX01/App.js
@@ -0,0 +1,18 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View>
+      <View>
+        <View></View>
+        <View></View>
+        <View></View>
+      </View>
+      <View></View>
+      <View></View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({});
