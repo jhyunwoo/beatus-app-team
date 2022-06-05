3c903a47b5f43c1787c347922ddbc308f57912c4
전현우 <jhyunwoo@Ampersand.local>
2022-06-06 01:56:36 +0900

    rn day 6 ex 1 answer

--- React-Native/DAY06EX01/App.js
+++ React-Native/DAY06EX01/App.js
@@ -3,14 +3,14 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <View>
-        <View></View>
-        <View></View>
-        <View></View>
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 2, backgroundColor: "teal", flexDirection: "row" }}>
+        <View style={{ flex: 1, backgroundColor: "blue" }}></View>
+        <View style={{ flex: 1, backgroundColor: "white" }}></View>
+        <View style={{ flex: 1, backgroundColor: "red" }}></View>
       </View>
-      <View></View>
-      <View></View>
+      <View style={{ flex: 2, backgroundColor: "tomato" }}></View>
+      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
     </View>
   );
 }
