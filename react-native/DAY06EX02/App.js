77aae922bfb17730b92afafe5214b7dd1155ce1a
jhyunwoo <tiers.edamame0z@icloud.com>
2022-06-14 02:26:07 +0900

    add day 6 ex 2 answer

--- react-native/DAY06EX02/App.js
+++ react-native/DAY06EX02/App.js
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <View style={{ flex: 2, backgroundColor: "teal" }}>
+        <View style={{ flex: 1 }}></View>
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          <View style={{ flex: 1, backgroundColor: "blue" }}></View>
+          <View style={{ flex: 1, backgroundColor: "white" }}></View>
+          <View style={{ flex: 1, backgroundColor: "red" }}></View>
+        </View>
+      </View>
+      <View style={{ flex: 2, backgroundColor: "tomato" }}></View>
+      <View style={{ flex: 1, backgroundColor: "orange" }}></View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
